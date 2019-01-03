bb2a18dd96fc9cc4854bea191c27d4f5ed1d9aee
刘元喆 <liuyuanzhe@ip-84-122.wireless.oberlin.edu>
2019-01-03 16:09:21 -0500

    First attempt of ce.py, only containing a printing line

---
+++ ce.py
@@ -0,0 +1,13 @@
+#ce.py
+#
+#ce --- chess engine: a program that can play the chess
+#   --- only following the basic rules of chess for now
+#
+#Author: Xuran Wang   Yuanzhe Liu
+#
+#2019/01/03
+
+def main():
+	print("Hello World!")
+
+main()
