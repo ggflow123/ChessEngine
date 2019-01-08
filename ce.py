ed29c617c125948a4cd25a8b06014a2b85aa1679
Xuran Wang <xwang2@oberlin.edu>
2019-01-07 21:22:38 -0500

    random command-line game

--- ce.py
+++ ce.py
@@ -8,22 +8,36 @@
 #2019/01/4
 
 import chess
+import random
 
 def main():
-    print("Hello World!!")
     board = chess.Board()
-    while not board.is_game_over:
+    while True:
         print(board)
-        if(board.turn):
-            # if it is white(lower)'s turn
+        print(board.fullmove_number)
+        if board.is_game_over():
+            break
+        elif(board.turn):
+            # if white(lower)'s turn
+            print("Please enter the move\n")
+            fromm = eval(input("from\n"))
+            too = eval(input("to\n"))
+            move = chess.Move(fromm,too)
+            if board.is_legal(move):
+                board.push(move)
+            else:
+                print("Invalid Input")
+                continue
         else:
-            # if it is black(upper)'s turn
-    print(result)
+            # if black's turn
+            index = random.randint(0,board.legal_moves.count())
+            the_iter=iter(board.legal_moves)
+            move=next(the_iter)
+            for i in range(1,index-1):
+                move=next(the_iter)
+            board.push(move)
 
-
-    print(board)
-    mv = chess.Move(8,16)
-    board.push(mv)
-    print(board)
+    print(board.result)
+    board.clear()
 
 main()
