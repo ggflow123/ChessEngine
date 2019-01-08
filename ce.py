cf1c80673be1ffe044c85c5cb1efc38b27e3faf8
Xuran Wang <xwang2@oberlin.edu>
2019-01-08 11:34:39 -0500

    finished the func of ab pruning

--- ce.py
+++ ce.py
@@ -10,7 +10,42 @@
 import chess
 import random
 
+# the evaluation function
+def eval_func(board):
+    return
+
+# the value of search value
+def ab_search(board,a,b,depth):
+    if depth==0:
+        return eval_func(board)
+    elif board.turn:
+        # if white
+        v=-10000
+        for move in board.legal_moves:
+            board.push(move)
+            v=max(v,ab_search(board,a,b,depth-1),a,b)
+            if v>=b:
+                board.pop()
+                return v
+            a=max(a,v)
+            board.pop()
+        return v
+    else:
+        # if black
+        v=10000
+        for move in board.legal_moves:
+            board.push(move)
+            v=min(v,ab_search(board,a,b,depth-1),a,b)
+            if v<=a:
+                board.pop()
+                return v
+            b=min(b,v)
+            board.pop()
+        return v
+
+
 def main():
+    depth=2
     board = chess.Board()
     while True:
         print(board)
@@ -30,12 +65,27 @@ def main():
                 continue
         else:
             # if black's turn
-            index = random.randint(0,board.legal_moves.count())
-            the_iter=iter(board.legal_moves)
-            move=next(the_iter)
-            for i in range(1,index-1):
-                move=next(the_iter)
-            board.push(move)
+
+            # make random move
+            #index = random.randint(0,board.legal_moves.count())
+            #the_iter=iter(board.legal_moves)
+            #move=next(the_iter)
+            #for i in range(1,index-1):
+            #    move=next(the_iter)
+            
+            # choose move that has the best value(min)
+            minvalue=10000
+            bestmove=chess.Move(0,1)
+            
+            for move in board.legal_moves:
+                board.push(move)
+                value=ab_search(-10000,10000,board,depth)
+                if value<=minvalue:
+                    bestmove=move
+                    minvalue=value
+                board.pop()
+
+            board.push(bestmove)
 
     print(board.result)
     board.clear()
